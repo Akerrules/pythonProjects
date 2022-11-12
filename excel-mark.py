a4f1ffcaf67af25f652c6efed0786fe55745f612
Akerrules <addrido@gmail.com>
2022-11-12 11:12:27 -0500

    partial mark logic fix

--- excel-mark.py
+++ excel-mark.py
@@ -18,16 +18,22 @@ with open('testanswer.csv', newline='') as csvfile:
         REAL_TEST_ANSWER.append(row[0].split(","))
 
 #calculate partial marks for each student
-resultCount = 0
 for student in  STUDENT_ANSWER:
-    studentMARK =0
-
+    studentMARK =0 # we assume each student got 0
     for index, q in enumerate(REAL_TEST_ANSWER):
-        for answer in q[0]: # go through each answer choice and check if it exist in the student choices
-           if(answer in student[index+1]):
-                resultCount= resultCount+1
-       #print("question: ", index+1 ,student[0], student[index+1],resultCount/len(q[1]), q[1])  
-        studentMARK += resultCount/len(q[0])  
+        resultCount = 0 # keep track of the point earned for each question
+        if((student[index+1].isnumeric())): # CHECKING  IF THE ANSWER IS A NUMBER VALUE. FOR EXAMPLE ONE OF THEM IS "*" in the student answer csv file
+            if(int(q[1])==1): # if number of choices is only one then we can just do exact match
+                if(int(q[0])==int(student[index+1])): resultCount=resultCount+1
+            else:
+                for answer in student[index+1]: # go through each answer choice and check if it exist in the student choices
+                    if(answer in q[0]):
+                        resultCount= resultCount+(float(1)/float(len(q[0]))) # for every correct answer we add the points 
+                    else:
+                        resultCount= resultCount-(float(1)/float(len(q[0]))) # for every incorrect choice we subtract the points
+                    if(resultCount<0): resultCount=0 # we can't have negative points
+       # print("question: ", index+1 ,student[0], student[index+1],resultCount, q[0], q[1])  
+        studentMARK += resultCount  
         resultCount=0
        
     testResult[str(student[0])] = studentMARK
